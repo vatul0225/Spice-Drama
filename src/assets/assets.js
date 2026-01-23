54b79d9adb1e118853d7e64dfedf8306c5bc9a69
Atul Verma <atulverma0225@gmail.com>
2026-01-24 04:20:31 +0530

    Remove hardcoded localhost API URL

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -124,4 +124,6 @@ export const menu_list = [
   },
 ];
 
-export const url = "http://localhost:4000";
+// export const url = "http://localhost:4000";
+
+export const url = import.meta.env.VITE_API_URL;
