a63e4f666959eda7be10837bb3f04b1aba8da8e0
Atul Verma <atulverma0225@gmail.com>
2026-01-24 03:38:25 +0530

    Use VITE_API_URL for backend requests

--- src/Components/Context/StoreContext.jsx
+++ src/Components/Context/StoreContext.jsx
@@ -6,7 +6,10 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+
+  const url = import.meta.env.VITE_API_URL;  // for render deployment 
+
   const [token, setToken] = useState("");
 
   const [food_list, setFoodList] = useState([]);
